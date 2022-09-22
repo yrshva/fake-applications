5b02f5e60b92f70b0388bf2bebe68c2265b4fa56
jesjonesc <58461507+yrshva@users.noreply.github.com>
2022-09-22 23:40:07 +0100

    added form component

--- src/components/ApplicationForm.tsx
+++ src/components/ApplicationForm.tsx
@@ -1,4 +1,24 @@
-const ApplicationForm = () => {
-  return null;
+import { FormEventHandler } from "react";
+import EmailInput from "./EmailInput";
+import FirstNameInput from "./FirstNameInput";
+import LastNameInput from "./LastNameInput";
+import CountryInput from "./CountryInput";
+import "../styles/Form.css";
+import "../styles/Datalist.css";
+
+const ApplicationForm = (props: {
+  handleSubmit: FormEventHandler<HTMLFormElement>;
+}) => {
+  return (
+    <div className="applicationform">
+      <form onSubmit={props.handleSubmit}>
+        <FirstNameInput />
+        <LastNameInput />
+        <EmailInput />
+        <CountryInput />
+        <input type="submit" value="Submit" className="btn btn-primary" />
+      </form>
+    </div>
+  );
 };
 export default ApplicationForm;
