7890d9e42d9c11abbd3a9508797b17faa316843a
jesjonesc <58461507+yrshva@users.noreply.github.com>
2022-09-23 01:29:02 +0100

    added transitions

--- src/components/ApplicationForm.tsx
+++ src/components/ApplicationForm.tsx
@@ -1,23 +1,57 @@
-import { FormEventHandler } from "react";
+import { FormEventHandler, useRef, useState } from "react";
 import EmailInput from "./EmailInput";
 import FirstNameInput from "./FirstNameInput";
 import LastNameInput from "./LastNameInput";
 import CountryInput from "./CountryInput";
+import { CSSTransition } from "react-transition-group";
+
 import "../styles/Form.css";
 import "../styles/Datalist.css";
 
 const ApplicationForm = (props: {
   handleSubmit: FormEventHandler<HTMLFormElement>;
 }) => {
+  const [apply, setApply] = useState(false);
+  const [applyButton, setApplyButton] = useState(true);
+
+  const nodeRef = useRef<any>(null);
   return (
     <div className="applicationform">
-      <form onSubmit={props.handleSubmit}>
-        <FirstNameInput />
-        <LastNameInput />
-        <EmailInput />
-        <CountryInput />
-        <input type="submit" value="Submit" className="btn btn-primary" />
-      </form>
+      {applyButton && (
+        <button
+          className="btn btn-primary"
+          type="button"
+          onClick={() => setApply(true)}
+        >
+          New Application
+        </button>
+      )}
+      <CSSTransition
+        nodeRef={nodeRef}
+        in={apply}
+        timeout={300}
+        unmountOnExit
+        onEnter={() => setApplyButton(false)}
+        onExited={() => setApplyButton(true)}
+        classNames="application"
+      >
+        <div ref={nodeRef}>
+          <button
+            className="btn alignright"
+            type="button"
+            onClick={() => setApply(false)}
+          >
+            X
+          </button>
+          <form onSubmit={props.handleSubmit}>
+            <FirstNameInput />
+            <LastNameInput />
+            <EmailInput />
+            <CountryInput />
+            <input type="submit" value="Submit" className="btn btn-success" />
+          </form>
+        </div>
+      </CSSTransition>
     </div>
   );
 };
